9ee7967c7c1ba79dc2f0368b3dd1efd242d3d160
ikeracv28 <iker.acevedo24@campusfp.es>
2025-10-06 18:24:24 +0200

    Orgaanizacion bien hecha

---
+++ CodigoApuntesProfe-U1/src/RutasEjemplo.java
@@ -0,0 +1,13 @@
+import java.io.File;
+public class RutasEjemplo {
+    public static void main(String[] args) {
+        String base = System.getProperty("user.dir");
+        String sep = File.separator;
+
+        String rutaAbsoluta = base + sep + "datos" + sep + "ejemplo.txt";
+        System.out.println("Ruta absoluta generada: " + rutaAbsoluta);
+
+        File archivo = new File(rutaAbsoluta);
+        System.out.println("¿Existe el archivo? " + archivo.exists());
+    }
+}
